297f24291be795aa8332c6ee7469a99d7dd126c2
Parsa.eissazadeh <parsa.eissazadeh@snapp.cab>
2021-12-25 17:19:52 +0330

    first time handler calling

--- main.go
+++ main.go
@@ -2,17 +2,56 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/nats-io/nats.go"
 )
 
-// Connect to a server
-nc, _ := nats.Connect(nats.DefaultURL)
+func main() {
+	// Connect to a server
+	ncSub, err := nats.Connect(nats.DefaultURL,
+		nats.DisconnectErrHandler(func(_ *nats.Conn, err error) {
+			println("client disconnected: %v", err)
+		}),
+		nats.ReconnectHandler(func(_ *nats.Conn) {
+			println("client reconnected")
+		}),
+		nats.ClosedHandler(func(_ *nats.Conn) {
+			println("client closed")
+		}))
 
-// Simple Publisher
-nc.Publish("foo", []byte("Hello World"))
+	if err != nil {
+		print("error in ncSub ")
+		println(err.Error())
+	}
 
-// Simple Async Subscriber
-nc.Subscribe("foo", func(m *nats.Msg) {
-	fmt.Printf("Received a message: %s\n", string(m.Data))
-})
+	// Simple Async Subscriber
+	ncSub.Subscribe("foo", func(m *nats.Msg) {
+		fmt.Printf("Received a message: %s\n", string(m.Data))
+	})
+
+	//----------------------------------------------------------------------
+
+	nc, err := nats.Connect(nats.DefaultURL,
+		nats.DisconnectErrHandler(func(_ *nats.Conn, err error) {
+			println("client disconnected: %v", err)
+		}),
+		nats.ReconnectHandler(func(_ *nats.Conn) {
+			println("client reconnected")
+		}),
+		nats.ClosedHandler(func(_ *nats.Conn) {
+			println("client closed")
+		}))
+
+	if err != nil {
+		print("error in nc ")
+		println(err.Error())
+	}
+
+	// Simple Publisher
+	nc.Publish("foo", []byte("Hello World"))
+
+	nc.Close()
+
+	runtime.Goexit()
+}
