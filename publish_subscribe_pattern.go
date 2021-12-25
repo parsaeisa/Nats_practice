7989f45081d53457c1e63d01e4e459bbae44f4b3
Parsa.eissazadeh <parsa.eissazadeh@snapp.cab>
2021-12-25 16:45:28 +0330

    add first publish subscribe

---
+++ publish_subscribe_pattern.go
@@ -0,0 +1,18 @@
+package Nats_practice
+
+import (
+	"fmt"
+
+	"github.com/nats-io/nats.go"
+)
+
+// Connect to a server
+nc, _ := nats.Connect(nats.DefaultURL)
+
+// Simple Publisher
+nc.Publish("foo", []byte("Hello World"))
+
+// Simple Async Subscriber
+nc.Subscribe("foo", func(m *nats.Msg) {
+	fmt.Printf("Received a message: %s\n", string(m.Data))
+})
